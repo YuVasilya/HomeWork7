e69995bc38891bed770ea59b9d20c31a6797d2e7
Vasilya <lady_drakula_94@mail.ru>
2022-10-13 22:13:53 +0500

    Ex01 done

---
+++ Ex01/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+double[,] CreateMatrix (int m, int n)
+{
+    return new double[m, n];
+}
+
+void FillMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = Math.Round(new Random().NextDouble()*20-10, 1);
+        }
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] matrix = CreateMatrix(3, 4);
+FillMatrix(matrix);
+PrintMatrix(matrix);
